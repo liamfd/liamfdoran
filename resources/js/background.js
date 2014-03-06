4a54a8e18a16576a7567de88c60df773d694b9e2
liamfd <liamfd@gmail.com>
2014-03-06 17:20:41 -0500

    switched to flexslider

--- resources/js/background.js
+++ resources/js/background.js
@@ -2,6 +2,24 @@ $( document ).ready(function() {
 //	alert("working!");
 	$('#blue').click(function(){
 		//alert(this.innerText)
-		$('.blueDiv').css("width", "100%")
+		//$('.blueDiv').css("width", "100%")
 	});
+	$(window).resize(function(){
+		checkWindowWidth();
+	});
+	
+});
+
+function checkWindowWidth(){
+	width = $(window).width();
+	if (width == 800){
+		console.log(width);
+	}
+
+}
+
+$(window).load(function() {
+    $(".flexslider").flexslider({
+        animation: "slide",
+    });
 });
